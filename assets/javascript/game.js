30911d225e88eacdc5bc13dfaa1ddb60e4a59a48
MaxMagic <Lennox.E.David@gmail.com>
2019-01-27 22:27:54 -0500

    starter

---
+++ assets/javascript/game.js
@@ -0,0 +1,38 @@
+/*
+Display the following on the page:
+
+
+Press any key to get started!
+
+
+Wins: (# of times user guessed the word correctly).
+
+
+If the word is madonna, display it like this when the game starts: _ _ _ _ _ _ _.
+
+
+As the user guesses the correct letters, reveal them: m a d o _ _ a.
+
+
+Number of Guesses Remaining: (# of guesses remaining for the user).
+
+
+Letters Already Guessed: (Letters the user has guessed, displayed like L Z Y H).
+
+
+After the user wins/loses the game should automatically choose another word and make the user play it.
+
+*/
+var words = ["marty", "doc", "delorean"];
+
+var game = {
+    start: function(){
+        document.getElementById("textBox").innerText("Press any key to get started!");
+        document.onkeyup(this.play());
+    },
+    play: function(){
+        var number = Math.floor(Math.random() * 2);
+        var answer = words[number];
+
+    }
+}
